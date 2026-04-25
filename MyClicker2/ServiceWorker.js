3094e1331f14dac74888688ed8e716117c7788ba
しょぽん <naminekana.shopon4646@gmail.com>
2026-04-25 09:11:31 +0900

    追加

---
+++ MyClicker2/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-MyClicker-1.0";
+const contentToCache = [
+    "Build/MyClicker_WebGL5.loader.js",
+    "Build/MyClicker_WebGL5.framework.js",
+    "Build/MyClicker_WebGL5.data",
+    "Build/MyClicker_WebGL5.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
